89e82a00e32d0759e25ffebf667afbee74fd8398
Amy Kistler <amy.kistler@czbiohub.org>
2019-01-21 13:50:19 -0800

    most updated version of code with verbose commenting, and specific example for README.txt

--- how_tos/ContigPull/contigPull.sh
+++ how_tos/ContigPull/contigPull.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 { read -r var1 ; read -r var2 ; } < sample_listFile;
 #echo "$var1 $var2"
-
-
-#while read -r ONE; 
-#do 
-    #read -r TWO
-    #echo "ONE: $ONE TWO: $RWO"
-#done < sample_listFile
-
-
-
-#while read LINE; do echo "$LINE"; done < sample_listFile
-#while IFS= read -r lineA && IFS= read -r lineB <&3; do
-    #echo "$lineA"; echo "$lineB"
-cat ../sequences/$var1 | seqkit grep -f $(basename $var1 contigs.fasta)$var2.csv  > ../sequences/$(basename $var1 contigs.fasta)$var2.fasta
+cat ../sequences/$var1 | seqkit grep -f $(basename $var1 contigs.fasta)$var2.txt  > ../sequences/$(basename $var1 contigs.fasta)$var2.fasta
 #done <sample_listFile 3<taxID_list
 
