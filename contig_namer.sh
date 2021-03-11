c17771ca5a4d6c4bcb3da18d76728e63fa74a8fb
Amy Kistler <amy.kistler@czbiohub.org>
2021-03-10 18:08:03 -0800

    cleaning up contig_namer.sh move to how_tos

--- contig_namer.sh
+++
@@ -1,9 +0,0 @@
-## FILE FOR RENAMING CONTIG FILES!! 
-for sample in `ls ./cycle_12/*cycle12.fasta`
-  do
-    dir="./"
-    base=$(basename $sample "_contigs.cycle12.fasta")
-      #subsample
-            #turn files into .fq
-    perl -p -e "s/^>/>${base}_/g" $sample > test.out; sed 's/[()\[]//g;s/\]//g' test.out > test2.out; sed 's/ /_/g' test2.out > ./contigs_named/${base}_contigs.out
-  done
